5983778629cd366a8c19b1d5f8c2faf226567dfa
Isaac Alter <i_alter@yahoo.com>
2023-01-30 12:02:21 -0700

    Adds feat: model testing as part of the task manager lesson in class

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,13 @@
 # app/models/task.rb
 
 class Task < ApplicationRecord
-    
+  def laundry?
+    if title == "laundry"
+      return true
+    elsif description == "laundry"
+      return true
+    else
+      return false
+    end
+  end
 end
